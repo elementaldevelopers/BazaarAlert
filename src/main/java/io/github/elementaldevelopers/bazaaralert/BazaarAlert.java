3578b4ce29acbf871cb1870f690c97d592079bf1
MySixSenses <66916362+MySixSenses@users.noreply.github.com>
2021-01-19 19:06:35 -0800

    First Commit

---
+++ src/main/java/io/github/elementaldevelopers/bazaaralert/BazaarAlert.java
@@ -0,0 +1,40 @@
+package io.github.elementaldevelopers.bazaaralert;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.PositionedSoundRecord;
+import net.minecraft.client.audio.SoundManager;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.sound.SoundEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.event.entity.PlaySoundAtEntityEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.lang.ref.Reference;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Mod(modid = BazaarAlert.MODID, version = BazaarAlert.VERSION)
+public class BazaarAlert {
+    public static final String MODID = "BazaarAlert";
+    public static final String VERSION = "1.0.0";
+    public static Pattern regex = Pattern.compile("^\\[Bazaar\\] Your (Sell|Buy) (Offer|Order) for \\d+x (\\w|\\s)+ was filled!$");
+	@EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    @SubscribeEvent
+    public void ChatRecieved(ClientChatReceivedEvent event) {
+        String message = event.message.getUnformattedText();
+    	Matcher m = regex.matcher(message);
+    	if (m.matches()) {
+    	    Minecraft.getMinecraft().getSoundHandler().playSound(PositionedSoundRecord.create(new ResourceLocation(this.MODID, "sound"), 1.0F));
+    	}
+    }  
+}
